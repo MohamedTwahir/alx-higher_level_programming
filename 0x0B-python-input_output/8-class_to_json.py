c2955090b6218475d08bba59c699fed0b07301f7
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-12-06 08:44:10 -0800

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creating a function that returns a dictionary description"""
+
+
+def class_to_json(obj):
+    """
+    Function that implements __dict__ to return a dictionary description
+    """
+    return obi.__dict__
