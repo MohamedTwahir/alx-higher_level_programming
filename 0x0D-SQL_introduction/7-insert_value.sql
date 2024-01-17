4bcebe525ba3c8d50666cb3f18abbe4449059653
MohamedTwahir <mohamedtwahir977@gmail.com>
2024-01-17 14:40:00 -0800

    First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- script that inserts a new row in the table first_table in you server
+-- new row id = 89 name = Best School
+-- database name will be passed on as an argument
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
