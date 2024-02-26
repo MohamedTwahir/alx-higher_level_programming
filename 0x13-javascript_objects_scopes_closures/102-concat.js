90531782eb98f749c7d41ddc0c5361b4a101f406
MohamedTwahir <mohamedtwahir977@gmail.com>
2024-02-26 06:08:58 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,28 +3,10 @@
 const fs = require('fs').promises;
 const { argv } = require('process');
 
-if (argv.length !== 5) {
-  console.error('Usage: ./script.js source_file1 source_file2 destination_file');
-  process.exit(1);
-}
+fs.readFile(argv[2], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, 'utf8'))
+  .catch(err => console.error(err));
 
-const sourceFile1 = argv[2];
-const sourceFile2 = argv[3];
-const destinationFile = argv[4];
-
-fs.readFile(sourceFile1, 'utf8')
-  .then(data => {
-    return fs.writeFile(destinationFile, data, 'utf8');
-  })
-  .then(() => {
-    return fs.readFile(sourceFile2, 'utf8');
-  })
-  .then(data => {
-    return fs.writeFile(destinationFile, data, { flag: 'a' }, 'utf8');
-  })
-  .then(() => {
-    console.log('Files concatenated successfully.');
-  })
-  .catch(err => {
-    console.error(err);
-  });
+fs.readFile(argv[3], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
+  .catch(err => console.error(err));
